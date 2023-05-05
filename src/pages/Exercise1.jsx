d437613443d8a5e7ab4f7676b48bfd8cfb0da794
Joaquin Garcia <joaquingm@MacBook-Pro-de-Joaquin.local>
2023-05-05 19:51:45 +0200

    Exercise 1, wip mock request of min max

--- src/pages/Exercise1.jsx
+++ src/pages/Exercise1.jsx
@@ -2,34 +2,14 @@ import { Range } from "../components/Range";
 
 export function Exercise1() {
   return (
-    <div>
-      <Range
-        config={{
-          min: 0,
-          max: 100,
-          showStepTicks: false,
-          currencyMode: true,
-          step: 0.01,
-        }}
-      ></Range>
-      <Range
-        config={{
-          min: 0,
-          max: 100,
-          showStepTicks: false,
-          currencyMode: true,
-          step: 0.01,
-        }}
-      ></Range>
-    </div>
-    //   <Range
-    //     config={{
-    //       min: 0,
-    //       max: 100,
-    //       showStepTicks: false,
-    //       currencyMode: true,
-    //       step: 0.01,
-    //     }}
-    //   ></Range>
+    <Range
+      config={{
+        min: 0,
+        max: 100,
+        showStepTicks: false,
+        currencyMode: true,
+        step: 0.01,
+      }}
+    ></Range>
   );
 }
