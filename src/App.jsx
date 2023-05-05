0a6ce3863a9b971201170187ddbf6583a47bfedd
Joaquin Garcia <joaquingm@MacBook-Pro-de-Joaquin.local>
2023-05-05 14:45:54 +0200

    Normal range bullets moving behavior and value selecting working, WIP change min,max

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
+import { Fragment } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Exercise1 } from "./pages/Exercise1";
+import "./styles/styles.css";
+
 export function App() {
-  return <h1>Range element!</h1>;
+  const routes = [
+    {
+      path: "/exercise1",
+      index: true,
+      element: <Exercise1 />,
+      errorElement: <h2>Page not found, please return.</h2>,
+    },
+  ];
+  const router = createBrowserRouter(routes);
+
+  return (
+    <Fragment>
+      <RouterProvider router={router} />
+    </Fragment>
+  );
 }
